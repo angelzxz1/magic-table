034d1a373fb0faabdce686f00bcd92568a4d250c
Angel Zuniga <angel.zuna@icloud.com>
2025-05-27 01:23:34 -0500

    idk man

---
+++ paths/index.ts
@@ -0,0 +1,10 @@
+export const paths = {
+    api: {
+        cookie: "/api/cookie",
+        user: "/api/user",
+        decks: {
+            deck: "/api/decks/deck",
+            decks: "/api/decks",
+        },
+    },
+};
